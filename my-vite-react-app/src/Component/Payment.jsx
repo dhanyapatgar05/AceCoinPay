9b856c41ecad006afdca61af06a5169806550a0e
dhanyapatgar05 <dhanyapatgar05@gmail.com>
2025-02-12 11:47:04 +0530

    Initial commit

--- my-vite-react-app/src/Component/Payment.jsx
+++ my-vite-react-app/src/Component/Payment.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { FaRegEdit } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
 import { RiVerifiedBadgeFill } from "react-icons/ri";
-
+import { BsFillGrid3X3GapFill, BsGrid3X3Gap } from "react-icons/bs";
 import "./Payment.css";
 
 
@@ -15,26 +15,76 @@ const Payment = () => {
           <span className="logo-text">AceCoin<span className="bold">Pay</span></span>
         </h2>
         <div className="timer">
-          <span>01</span>:<span>19</span>
+          <span className="timer-box">0</span>
+          <span className="timer-box">1</span>
+          <span className="timer-separator">:</span>
+          <span className="timer-box">1</span>
+          <span className="timer-box">9</span>
         </div>
       </div>
       
-      {/* Card Number Section */}
+      {/* Card Section */}
       <div className="card-section">
         <div className="card-header">
           <label className="card-label">Card Number</label>
           <div className="edit-option">
-            <FaRegEdit className="edit-icon" /> <span>Edit</span>
+            <MdEdit className="edit-icon" /> <span>Edit</span>
           </div>
         </div>
         <p className="card-description">Enter the 16-digit card number on the card</p>
+        {/* Card Number Section */}
         <div className="card-display">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPMEm-efiVHsW0NsLe3lZ7lixMo3vJhGhO0w&s" alt="Card Logo" className="card-logo" />
-          <span className="card-number">2412 - 7512 - 3412 - 3456</span>
+          <div className="card-number">
+            <span>2412</span>
+            <span>-</span>
+            <span>7512</span>
+            <span>-</span>
+            <span>3412</span>
+            <span>-</span>
+            <span>3456</span>
+          </div>
           <div className="edit-icon-wrapper">
             <RiVerifiedBadgeFill />
           </div>
         </div>
+      </div>
+      <div className="payment-form">
+      {/* CVV Number */}
+        <div className="cvv-container">
+          <div className="cvv-label">
+            <label htmlFor="cvv">CVV Number</label>
+            <p className="cvv-description">Enter the 3 or 4 digit number on the card</p>
+          </div>
+          <div className="cvv-input-box">
+            <input type="password" id="cvv" placeholder="327" maxLength="4" />
+            <BsFillGrid3X3GapFill  className="cvv-icon" />
+          </div>
+        </div>
+        {/* Expiry Date */}
+        <div className="expiry-container">
+          <div className="expiry-label">
+            <label htmlFor="expiry">Expiry Date</label>
+            <p className="expiry-description">Enter the expiration date of the card</p>
+          </div>
+          <div className="expiry-input-box">
+            <input type="text" id="month" placeholder="09" maxLength="2" />
+            <span className="expiry-separator">/</span>
+            <input type="text" id="year" placeholder="22" maxLength="2" />
+          </div>
+        </div>
+
+
+
+       
+        <div className="input-group">
+          <label>Password</label>
+            <div className="input-box">
+              <input type="password" placeholder="Enter Password" />
+              <BsGrid3X3Gap className="input-icon" />
+            </div>
+        </div>
+        <button className="pay-now-btn">Pay Now</button>
       </div>
     </div>
   );
